a77c83db5e1e9569d9c4d6b6f37378d62970fbac
CHAN QUANG <quangtrinhhuynh02@gmail.com>
2023-09-28 00:24:38 +0700

    deploy

--- src/main/java/com/twinkle/shopapp/utils/EmailUtils.java
+++ src/main/java/com/twinkle/shopapp/utils/EmailUtils.java
@@ -38,8 +38,8 @@ public class EmailUtils {
     // Email: danuiluahongquang@gmail.com
     // Password: eerapsmzyzmkuljz
 
-    final static String from = "danuiluahongquang@gmail.com";
-    final static String password = "gigh umpj xiwz nrkw";
+    final static String from = "quangtrinhhuynh02@gmail.com";
+    final static String password = "hepn yoyz ovqz josj";
 
     public static void sendEmail(String to, String tieuDe, String noiDung) {
 
